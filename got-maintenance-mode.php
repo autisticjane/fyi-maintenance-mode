4a7b0a6ca8873b7aa70087f51657c81ed847f496
Jane Lawson <git@jane.fyi>
2019-01-23 19:38:56 -0600

    Update got-maintenance-mode.php

--- got-maintenance-mode.php
+++ got-maintenance-mode.php
@@ -1,3 +1,17 @@
+<?php
+/*
+    Plugin Name: Simple Maintenance Mode
+    Plugin URI: https://janepedia.com
+    Description: A basic maintenance mode plugin for WordPress if you're not interested in all the bells and whistles of other plugins.
+    Author: Jane Lawson
+    Author URI: https://janepedia.com
+    Version: 1.0
+*/
+// if someone attempts to visit plugin directly, they will see a poop emoji
+    if ( !function_exists( 'add_action' ) ) {
+        echo '&#128169;';
+        exit;
+    }
 // Activate WordPress Maintenance Mode
 function wp_maintenance_mode() {
     if (!current_user_can('edit_themes') || !is_user_logged_in()) {
@@ -6,3 +20,4 @@ function wp_maintenance_mode() {
     }
 }
 add_action('get_header', 'wp_maintenance_mode');
+?>
