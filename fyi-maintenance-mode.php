af5255f104c9ee7ebd456598148b6a0132f24456
Jane Lawson <gotjane@users.noreply.github.com>
2019-01-24 18:41:33 -0600

    Update fyi-maintenance-mode.php
    Changed <br /> to <p>

--- fyi-maintenance-mode.php
+++ fyi-maintenance-mode.php
@@ -16,7 +16,7 @@
 function wp_maintenance_mode() {
     if (!current_user_can('edit_themes') || !is_user_logged_in()) {
         wp_die('<h1>Under Maintenance</h1>
-		<br />Be back whenever.');
+		<p>Be back whenever.</p>');
     }
 }
 add_action('get_header', 'wp_maintenance_mode');
